f9a3f778551fe0be5475f8736f6024b9ba381a32
horden316 <horden316@gmail.com>
2022-10-25 15:27:17 +0800

    feat: add writefile example

--- emovest_back/binance/trade_history.js
+++ emovest_back/binance/trade_history.js
@@ -1,29 +1,35 @@
-const Binance = require('node-binance-api');
-const Keys    = require('./config.js');
+const Binance = require("node-binance-api");
+const Keys = require("./config.js");
 const binance = new Binance().options(Keys);
-var fs = require('fs')
+var fs = require("fs");
 
-let options = { 
-    limit: 2 // Limit results to two entries
-  };
-
-
+let options = {
+  limit: 2, // Limit results to two entries
+};
 
 //trade 歷史紀錄
-binance.trades("ETHUSDT", (error, trades, symbol) => {  
-    for(let k in trades) { 
-    let trade = trades[k];
-    let tradeDateTime = new Date(trade.time);
-    console.log(`Trade event --------------------------------`);
-    console.log(`time             : ${tradeDateTime}`);
-    console.log(`symbol           : ${trade.symbol}`);
-    console.log(`price            : ${trade.price}`);
-    console.log(`quantity         : ${trade.qty}`);
-    console.log(`commission fee   : ${trade.commission}`);
-    console.log(`commission asset : ${trade.commissionAsset}`);
-//    console.log(trade);
+binance.trades(
+  "ETHUSDT",
+  (error, trades, symbol) => {
+    fs.writeFile("./trade.json", JSON.stringify(trades), function (error) {
+      console.log(error);
+      console.log("文件寫入成功");
+    });
+    for (let k in trades) {
+      let trade = trades[k];
+      let tradeDateTime = new Date(trade.time);
+      console.log(`Trade event --------------------------------`);
+      console.log(`time             : ${tradeDateTime}`);
+      console.log(`symbol           : ${trade.symbol}`);
+      console.log(`price            : ${trade.price}`);
+      console.log(`quantity         : ${trade.qty}`);
+      console.log(`commission fee   : ${trade.commission}`);
+      console.log(`commission asset : ${trade.commissionAsset}`);
+      //    console.log(trade);
     }
-}, options);
+  },
+  options
+);
 
 /*
 
@@ -38,9 +44,9 @@ binance.balance((error, balances) => {
   */
 
 binance.allOrders("ETHUSDT", (error, orders, symbol) => {
-  console.info(symbol+" orders:", JSON.stringify(orders));
-  fs.writeFile('./ETHUSDT.json', JSON.stringify(orders),function (error) {
-    console.log(error)
-    console.log('文件寫入成功')
-  })
+  console.info(symbol + " orders:", JSON.stringify(orders));
+  fs.writeFile("./ETHUSDT.json", JSON.stringify(orders), function (error) {
+    console.log(error);
+    console.log("文件寫入成功");
+  });
 });
