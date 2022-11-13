b58d246f1ea3f7a65902f9788103b450ad2f9584
horden316 <horden316@gmail.com>
2022-11-13 19:45:27 +0800

    feat: 新增平均成本查詢

--- emovest_back/binance/trade_history.js
+++ emovest_back/binance/trade_history.js
@@ -1,4 +1,5 @@
 const Binance = require("node-binance-api");
+const path = require("path");
 require("dotenv").config();
 const Keys = {
   APIKEY: process.env.binance_api_key,
@@ -11,42 +12,6 @@ let options = {
   limit: 2, // Limit results to two entries
 };
 
-//trade 歷史紀錄
-binance.trades(
-  "ETHUSDT",
-  (error, trades, symbol) => {
-    fs.writeFile("./trade.json", JSON.stringify(trades), function (error) {
-      console.log(error);
-      console.log("文件寫入成功");
-    });
-    for (let k in trades) {
-      let trade = trades[k];
-      let tradeDateTime = new Date(trade.time);
-      console.log(`Trade event --------------------------------`);
-      console.log(`time             : ${tradeDateTime}`);
-      console.log(`symbol           : ${trade.symbol}`);
-      console.log(`price            : ${trade.price}`);
-      console.log(`quantity         : ${trade.qty}`);
-      console.log(`commission fee   : ${trade.commission}`);
-      console.log(`commission asset : ${trade.commissionAsset}`);
-      //    console.log(trade);
-    }
-  },
-  options
-);
-
-/*
-
-
-//取得balance
-binance.balance((error, balances) => {
-    if ( error ) return console.error(error);
-    console.info("balances()" + JSON.stringify(balances));
-    console.info("ETH balance: ", balances.ETH.available);
-  });
-
-  */
-
 binance.allOrders("ETHUSDT", (error, orders, symbol) => {
   console.info(symbol + " orders:", JSON.stringify(orders));
   fs.writeFile("./ETHUSDT.json", JSON.stringify(orders), function (error) {
@@ -54,3 +19,46 @@ binance.allOrders("ETHUSDT", (error, orders, symbol) => {
     console.log("文件寫入成功");
   });
 });
+
+async function main() {
+  getAveragePrice("ETHUSDT", 31556926).then((v) => {
+    console.log(v);
+  });
+}
+
+async function getAllTradingPairAndPrice() {
+  let ticker = await binance.prices();
+  console.log(ticker);
+}
+
+async function getAveragePrice(TradingPair, ViewTime) {
+  //傳入要算的交易對的名稱, 還有要觀察的時間（用UnixTime）
+  const ALLOrder = JSON.parse(fs.readFileSync("./" + TradingPair + ".json"));
+
+  const dateTime = Date.now();
+  const timestamp = Math.floor(dateTime / 1000);
+
+  var TotalSpend = 0; //每單的價格*數量相加
+  var TotalQuan = 0; //每單的數量相加
+  for (var i = 0; i < ALLOrder.length; i++) {
+    if (
+      ALLOrder[i].updateTime > timestamp - ViewTime &&
+      ALLOrder[i].status == "FILLED" &&
+      ALLOrder[i].side == "BUY"
+    ) {
+      TotalQuan += Number(ALLOrder[i].executedQty);
+      TotalSpend += Number(ALLOrder[i].executedQty) * Number(ALLOrder[i].price);
+    }
+  }
+  console.log(TotalQuan);
+  console.log(TotalSpend);
+  var Ave = TotalSpend / TotalQuan;
+  return Ave;
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
