b1c11e3e27e3fbd43de5a1914333d64f648c1d10
horden316 <horden316@gmail.com>
2022-11-30 20:10:51 +0800

    fix: host name

--- SentimentAnalysis/write_result_sql.py
+++ SentimentAnalysis/write_result_sql.py
@@ -1,5 +1,5 @@
 import pymysql
-Coin = pymysql.connect('localhost', port=3306, user='root', passwd='', charset='utf8', db='analysis_result')
+Coin = pymysql.connect(host='localhost', port=3306, user='root', passwd='', charset='utf8', db='analysis_result')
 
 with Coin.cursor() as cursor:
     cursor.execute("INSERT INTO  `result_score_ave` (`Symbol`,`Score`) VALUE ('BTC','0.759999999999') ")
