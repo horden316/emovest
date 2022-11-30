3ee23d065166fbd5f86655272740328fb6ff32da
horden316 <horden316@gmail.com>
2022-11-30 16:54:17 +0800

    feat: add resultdict

--- SentimentAnalysis/analyze.py
+++ SentimentAnalysis/analyze.py
@@ -8,20 +8,24 @@ from official.nlp import optimization  # to create AdamW optimizer 優化算法
 
 import matplotlib.pyplot as plt #繪圖工具
 
+import statistics
+
 basepath=os.getcwd()
 saved_model_path = basepath+"/SentimentAnalysis/coinmarket_bert"
 reloaded_model = tf.saved_model.load(saved_model_path)
 
-coinsymbol=["BTC","ETH","BNB","XRP","DOGE","ADA","AVAX","TRX","SOL","DOT","LUNA"]
 
 
+coinsymbol=["BTC","ETH","BNB","XRP","ADA","SOL","DOGE","DOT","TRX","AVAX","LUNA"]
+resultdict={"BTC":0,"ETH":0,"BNB":0,"XRP":0,"ADA":0,"SOL":0,"DOGE":0,"DOT":0,"TRX":0,"AVAX":0,"LUNA":0}
 
-def print_my_score_without_example(results):
-    result_for_printing = \
-        [f'{results[i][0]:.6f}'
-                            for i in range(len(results))]
-    print(*result_for_printing, sep='\n')
-    print()
+
+# def print_my_score_without_example(results):
+#     result_for_printing = \
+#         [f'{results[i][0]:.6f}'
+#                             for i in range(len(results))]
+#     print(*result_for_printing, sep='\n')
+#     print()
 
 for j in range(len(coinsymbol)):
     path=basepath+"/SentimentAnalysis/data/"+coinsymbol[j]+".txt"
@@ -33,6 +37,15 @@ for j in range(len(coinsymbol)):
 
     print(coinsymbol[j] +' Results from the saved model:')
 
-    
+    result_for_printing = \
+        [f'{reloaded_results[i][0]:.6f}'
+                            for i in range(len(reloaded_results))]
+    #print(*result_for_printing, sep='\n')
 
-    print_my_score_without_example(reloaded_results)
+    mean = statistics.mean(result_for_printing)
+    resultdict[coinsymbol]=mean
+    
+print(resultdict)
+
+
+
