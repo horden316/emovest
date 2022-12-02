3902b89e4a7be216002f935da5a8f8888dd600e7
horden316 <horden316@gmail.com>
2022-12-02 13:43:12 +0800

    feat: get score every 4 hour

--- SentimentAnalysis/analyze.py
+++ SentimentAnalysis/analyze.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt #繪圖工具
 
 import statistics
 import numpy as np
-
+import time
 
 basepath=os.getcwd()
 saved_model_path = basepath+"/SentimentAnalysis/coinmarket_bert"
@@ -29,33 +29,39 @@ resultdict={"BTC":0,"ETH":0,"BNB":0,"XRP":0,"ADA":0,"SOL":0,"DOGE":0,"DOT":0,"TR
 #     print(*result_for_printing, sep='\n')
 #     print()
 
-for j in range(len(coinsymbol)):
-    path=basepath+"/SentimentAnalysis/data/"+coinsymbol[j]+".txt"
-    with open(path,"r") as f:
-        examples = f.readlines()
+while True:
+    #四小時抓一次
+    if time.time()-InitialTime > 14400:
+        print(time.time())
+        InitialTime=time.time()
+
+    for j in range(len(coinsymbol)):
+        path=basepath+"/SentimentAnalysis/data/"+coinsymbol[j]+".txt"
+        with open(path,"r") as f:
+            examples = f.readlines()
 
 
-    reloaded_results = tf.sigmoid(reloaded_model(tf.constant(examples)))
+        reloaded_results = tf.sigmoid(reloaded_model(tf.constant(examples)))
 
-    print(coinsymbol[j] +' Results from the saved model:')
+        print(coinsymbol[j] +' Results from the saved model:')
 
-    result_for_printing = \
-        [f'{reloaded_results[i][0]:.6f}'
-                            for i in range(len(reloaded_results))]
-    #print(*result_for_printing, sep='\n')
+        result_for_printing = \
+            [f'{reloaded_results[i][0]:.6f}'
+                                for i in range(len(reloaded_results))]
+        #print(*result_for_printing, sep='\n')
 
-    floatresult = list(np.float_(result_for_printing))
-    mean = statistics.mean(floatresult)
-    resultdict[coinsymbol[j]]=mean
-    
-print(resultdict)
+        floatresult = list(np.float_(result_for_printing))
+        mean = statistics.mean(floatresult)
+        resultdict[coinsymbol[j]]=mean
+        
+    print(resultdict)
 
-import pymysql
-Coin = pymysql.connect(host='localhost', port=3306, user='root', passwd='', charset='utf8', db='analysis_result')
+    import pymysql
+    Coin = pymysql.connect(host='localhost', port=3306, user='root', passwd='', charset='utf8', db='analysis_result')
 
-with Coin.cursor() as cursor:
-    for r in resultdict.items():
-        cursor.execute("UPDATE result_score_ave SET Score="+ str(r[1]) +" WHERE Symbol='"+str(r[0])+"'")
-        Coin.commit()
-Coin.close()
+    with Coin.cursor() as cursor:
+        for r in resultdict.items():
+            cursor.execute("UPDATE result_score_ave SET Score="+ str(r[1]) +" WHERE Symbol='"+str(r[0])+"'")
+            Coin.commit()
+    Coin.close()
 
