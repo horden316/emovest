515ee3f002df72eccedf51763a6baf1328b68cfe
horden316 <horden316@gmail.com>
2022-12-16 17:09:43 +0800

    feat: add bodyParser

--- emovest_back/EmovestServerExpress.js
+++ emovest_back/EmovestServerExpress.js
@@ -3,7 +3,10 @@ var cors = require("cors"); //cors 的預設為全開放
 var app = express();
 require("dotenv").config();
 var mysql = require("mysql");
+var bodyParser = require("body-parser");
 const port = 45001;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // connect MySQL
 var connection = mysql.createConnection({
@@ -19,7 +22,7 @@ function main() {
     res.send("hello world");
   });
 
-  app.post("/WalletSig", function (req, res) {
+  app.post("/WalletSig", urlencodedParser, function (req, res) {
     console.log(req.query.id);
     console.log(req.body.name);
     console.log(req.body.tel);
